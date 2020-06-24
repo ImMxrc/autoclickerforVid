6db5704f506aca498b29df60b9f23d8c1292c336
Swedish Twat <kalliokoski.elias@gmail.com>
2020-06-24 22:10:18 +0200

    Add project files.

---
+++ cl/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cl
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+static extern short GetAsyncKeyState(Keys vKey);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+        private const int LEFTUP = 0x0004;
+        private const int LEFTDOWN = 0x0002;
+        public int interval = 1;
+        public bool threads = false;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            backgroundWorker1.RunWorkerAsync();
+            Thread AC = new Thread(autoClicker);
+            AC.Start();
+            
+
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while (true)
+            {
+                if (checkBox1.Checked)
+                {
+                    if (GetAsyncKeyState(Keys.Down) < 0)
+                    {
+                        threads = false;
+
+                    }
+                    else if (GetAsyncKeyState(Keys.Up) < 0)
+                    {
+                        threads = true;
+                    }
+                    Thread.Sleep(1);
+                }
+                Thread.Sleep(1);
+            }
+        }
+        private void autoClicker()
+        {
+            while (true)
+            {
+                if (threads == true)
+                {
+                    mouse_event(dwFlags: LEFTUP, dx: 0, dy: 0, cButtons: 0, dwExtraInfo: 0);
+                    Thread.Sleep(1);
+                    mouse_event(dwFlags: LEFTDOWN, dx: 0, dy: 0, cButtons: 0, dwExtraInfo: 0);
+                    Thread.Sleep(interval);
+                }
+                Thread.Sleep(2);
+            }
+        }
+    }
+}
